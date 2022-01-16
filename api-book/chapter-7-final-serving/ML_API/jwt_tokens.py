d34405d80585aac097c487dcced146cc205a5453
Eligijus Bujokas <elasorama@gmail.com>
2022-01-16 10:05:22 +0200

    Feature/implementation (#41)
    * Added several chapters on user creation and authentification
    
    * Added a quick ML building chapter for the API
    
    * Some tweaks to the ML model
    
    * Added the request and responses table
    
    * Added some info to ML API
    
    * Added the chapter on machine learning API
    
    * Added the final chapter
    
    * Added the implementation in dokcer
    
    * Added the publishing package

---
+++ api-book/chapter-7-final-serving/ML_API/jwt_tokens.py
@@ -0,0 +1,117 @@
+# Importing the JWT library
+import jwt
+
+# The Users model 
+from Users import User
+
+# Importing the session 
+from database import session
+
+# Datetime functionality
+import datetime
+
+# Reading the configuration file 
+import yaml 
+
+# OS functionalities
+import os 
+
+# Infering the file path 
+_file_dir = os.path.dirname(os.path.abspath(__file__))
+
+conf = yaml.safe_load(open(os.path.join(_file_dir, "config.yml")))
+
+# JWT constants
+_SECRET = conf["jwt"]["secret"]
+_ALGORITHM = conf["jwt"]["algorithm"]
+_EXPIRATION_TIME = conf["jwt"]["expiration_time"] # Minutes until expiration
+
+# User authenfification endpoint 
+def authenticate_user_view(username: str, password: str) -> bool:
+    """
+    Function that authenticates a user using the username and password
+    
+    Parameters
+    ----------
+        username (str): The username of the user to authenticate
+        password (str): The password of the user to authenticate
+    
+    Returns
+    -------
+        bool: True if the user is authenticated, False otherwise
+    """
+    # Checking if the user exists in the database
+    user = session.query(User).filter(User.username == username).first()
+    if user:
+        # Checking if the password is correct
+        if user.check_password_match(password):
+            return user
+        else:
+            return None
+    else:
+        return None
+
+# Creating the JWT token
+def create_token_view(user_id: int) -> str:
+    """
+    Method to create a JWT token for a user using internal user_id
+    
+    Parameters
+    ----------
+        user_id (int): The user_id of the user to create the token for
+
+    Returns
+    -------
+        str: The JWT token
+    """
+    # Creating the claims dictionary
+    claims = {
+        # Expiration date of the token
+        "exp": datetime.datetime.now() + datetime.timedelta(minutes=_EXPIRATION_TIME),
+        
+        # Issue time of the token
+        "iat": datetime.datetime.now(),
+
+        # Subject of the token
+        "sub": user_id
+    }
+
+    # Creating the token
+    return jwt.encode(claims, _SECRET, algorithm=_ALGORITHM)
+
+# Authenticating the JWT token
+def authenticate_token_view(jwt_token: str) -> bool:
+    """
+    Function that decodes the token and authenticates it. 
+
+    Parameters
+    ----------
+        jwt_token (str): The JWT token to authenticate
+
+    Returns
+    -------
+        bool: True if the token is valid, False otherwise
+    """
+    try:
+        # Decoding the token
+        claims = jwt.decode(jwt_token, _SECRET, algorithms=[_ALGORITHM])
+
+        # Extracting the user_id from the token
+        user_id = claims["sub"]
+
+        # Extracting the expiration date from the token
+        expiration_date = claims["exp"]
+
+        # Checking if the token is expired
+        if datetime.datetime.fromtimestamp(expiration_date) < datetime.datetime.utcnow():
+            return False
+
+        # Checking if the user exists in the database
+        user = session.query(User).filter(User.id == user_id).first()
+        if user:
+            return user
+        else:
+            return None
+    except:
+        # If the token is invalid, return False
+        return None 
--- api-book/chapter-7-final-serving/ML_API_docker/database/Users.py
+++ api-book/chapter-7-final-serving/ML_API_docker/database/Users.py
@@ -16,6 +16,9 @@ import datetime
 # OS traversal 
 import os 
 
+# Importing the current session
+from database.database import session
+
 # Initiating the Base class
 Base = declarative_base()
 
@@ -104,3 +107,66 @@ class User(Base):
             return True
         else:
             return False
+
+def register_user_view(username: str, password: str, email: str):
+    """
+    Method for registering a user
+    """
+    # Checking if a certain user already exists 
+    _user = session.query(User).filter_by(username=username).first()
+
+    if _user:
+        return {"message": "User already exists", "user_id": _user.id}
+    else:
+        # Creating the user object
+        _user = User(username, password, email)
+
+        # Adding the user to the database
+        session.add(_user)
+        session.commit()
+
+        # Returning the user object
+        return _user
+
+def toggle_user_permission_view(user_id: int, permission: bool):
+    """
+    Method for disabling/enableing a user. 
+    """
+    # Getting the user object
+    _user = session.query(User).filter_by(id=user_id).first()
+
+    if _user:
+        # Converting the permission to boolean type 
+        try: 
+            permission = bool(permission)
+        except ValueError as e:
+            return {"message": f"Cannot convert permission to boolean. Error: {e}"}
+
+        # Setting the user status to disabled
+        _user.enabled = permission
+        _user.updated_datetime = datetime.datetime.now()
+
+        # Committing the changes to the database
+        session.commit()
+
+        # Returning the user object
+        return _user
+    else:
+        return None
+
+def remove_user_view(user_id: int):
+    """
+    Removes the user from the database
+    """
+    # Getting the user object
+    _user = session.query(User).filter_by(id=user_id).first()
+
+    if _user:
+        # Deleting the user object
+        session.delete(_user)
+        session.commit()
+
+        # Returning the user object
+        return _user
+    else:
+        return None
